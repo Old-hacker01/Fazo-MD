7a925655152a7889036dbea425513aec2c869d69
Fazo28 <Fatmacomputer83@gmail.com>
2025-07-01 10:12:37 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "FAZO-MD",
+    prefix: "!",
+    admins: ["255675007732"], // Add admin numbers without country code
+    welcomeMessage: "Welcome to the group!",
+    autoReply: true,
+    autoDownload: true,
+    statusViewer: true
+};
